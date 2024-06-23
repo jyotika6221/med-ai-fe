40ae1e34dde01ed9744ef6c131036ce24b1b30a0
Pooranjoy <pooranjoyb2016@gmail.com>
2024-06-24 00:08:23 +0530

    Added vercel.json

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 import "../App.css"
 
 function SignIn() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("admin");
+    const [password, setPassword] = useState("admin");
     const navigate = useNavigate();
 
     const handleSignIn = async (e) => {
